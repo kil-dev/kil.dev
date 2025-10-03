906f1c1bde9f17b493fb7a1a8f57c0908f53f27e
Kilian Tyler <kilian@kil.dev>
2025-10-02 22:36:12 -0400

    feat(nav): Add data-active attribute to NavLink for styling control

--- src/components/layout/header/nav-lava.tsx
+++ src/components/layout/header/nav-lava.tsx
@@ -325,7 +325,7 @@ function NavLink(props: NavLinkProps) {
           />
         </>
       )}
-      <span className="relative z-10">
+      <span className="relative z-10" data-active={isActive ? 'true' : 'false'}>
         {label}
         {showUnderline && (
           <span className="absolute -bottom-1 left-0 h-0.5 w-0 bg-current transition-all duration-300 ease-out group-hover:w-full" />
