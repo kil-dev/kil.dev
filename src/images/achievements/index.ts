b7a141664ef634b7954587b1714641f45d2310db
Kilian Tyler <kilian@kil.dev>
2025-10-06 21:00:04 -0400

    chore: unused image

--- src/images/achievements/index.ts
+++ src/images/achievements/index.ts
@@ -4,9 +4,8 @@ export { default as ExperienceExplorerAchievement } from './experience-explorer.
 export { default as FiveStarFanAchievement } from './fivestar-fan.webp'
 export { default as GrumpyGlimpseAchievement } from './grumpy-glimpse.webp'
 export { default as KonamiKillerAchievement } from './konami-killer.webp'
-export { default as LadybirdLandingAchievement } from './ladybird-landing.webp'
+// export { default as LadybirdLandingAchievement } from './ladybird-landing.webp'
 export { default as PetParadeAchievement } from './pet-parade.webp'
-
 export { default as ProjectsPeruserAchievement } from './projects-peruser.webp'
 export { default as RecursiveRewardAchievement } from './recursive-reward.webp'
 export { default as ThemeTapdanceAchievement } from './theme-tapdance.webp'
